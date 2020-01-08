89b079d47f6ef0f61b8d49b1acc030cf049128b9
lichaofeng <lichaofeng@qq.com>
2020-01-08 09:52:34 +0800

    -

---
+++ FundApp/Common/ExtensionObj.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundApp
+{
+    public static class ExtensionObj
+    {
+        //public static DateTime ToCstTime(this DateTime dateTime)
+        //{
+
+        //}
+             
+    }
+}
