15a3688e81fded753e99883c81002f2bd4a3daa7
lichaofeng <viplichaofeng@163.com>
2020-03-18 14:36:05 +0800

    Update index.vue

--- FundApp/src/views/fundList/index.vue
+++ FundApp/src/views/fundList/index.vue
@@ -5,6 +5,10 @@
             <iframe src="http://fundgz.1234567.com.cn/js/257070.js" style="border:0px; width:1100px; height:28px; overflow:hidden;" scrolling="no"></iframe>
             <iframe src="http://fundgz.1234567.com.cn/js/320007.js" style="border:0px; width:1024px; height:28px; overflow:hidden;" scrolling="no"></iframe>
         </div>
+        <hr />
+        <div>
+            <img v-for="p in fundCodes" :src="getSplice(p)" :key="p" style="width:450px;height:280px;"/>
+        </div>
     </div>
 </template>
 <script>
@@ -12,8 +16,15 @@
         name: 'HelloWorld',
         data() {
             return {
-                msg: 'fundList!!!'
+                fundCodes: ['320007', '007301','257070'],
+                msg: 'fundList!!!',
+
             }
+        },
+        methods: {
+            getSplice(code) {
+                return "http://j4.dfcfw.com/charts/pic6/" + code + ".png?v=" + Math.random()
+            },
         }
     }
 </script>
